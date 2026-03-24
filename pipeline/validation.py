1196c3adac4176dbc6dde7c3047046e791311a42
Lei Andrei Domaoal <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2026-03-24 22:43:30 +0800

    Additional: some comments and the same logic from extract module.

--- pipeline/validation.py
+++ pipeline/validation.py
@@ -1,4 +1,7 @@
+from utils.logger import log
 import pandas as pd
+
+logger = log(__name__)
 
 # still figuring out what to put in here
 
@@ -9,5 +12,26 @@ import pandas as pd
         - unstandardize strings/values
 
 """
-def describe_data(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+# same logic from the extract.py but it should return a clean data check
+def data_quality(clean_df: pd.DataFrame) -> pd.Series:
+    
+    logger.info("Data Quality Check from the DataFrame")
+
+    if clean_df.empty:
+        raise ValueError("The DataFrame is Empty.")
+
+    try:
+
+        quality = pd.Series({
+            "total_records" : len(clean_df),
+            "duplicated_rows" : int(clean_df.duplicated().sum()),
+            "missing_values" : int(clean_df.isna().sum().to_dict())
+        })
+
+        return quality
+
+    except Exception as e:
+        raise RuntimeError(f"Unexpected error during data inspection: {e}") from e
+
+    except ValueError as e:
+        raise RuntimeError(f"Invalid input: {e}") from e
